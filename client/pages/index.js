e770411b4fed2c2d45fe93b0ef827c5247d1ca80
alihassan-here <alihassanuog50@gmail.com>
2022-06-23 17:07:51 +0500

    initial setup

---
+++ client/pages/index.js
@@ -0,0 +1,15 @@
+
+
+const Home = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h1>Home Page</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ client/pages/login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h1>Login Page</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
