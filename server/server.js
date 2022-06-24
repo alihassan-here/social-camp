3d49a3d6be41d24a3ac67fb71c0a1e15c8182435
alihassan-here <alihassanuog50@gmail.com>
2022-06-24 22:47:17 +0500

    setup basic server

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import { readdirSync } from "fs";
+
+const morgan = require('morgan');
+require('dotenv').config();
+
+const app = express();
+
+//DB
+mongoose.connect(process.env.MONGOURI)
+    .then(() => console.log("DB CONNCECTED"))
+    .catch(err => console.log("DB CONNECTION ERROR=>", err));
+
+//MIDDLEWARES
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: ["http://localhost:3000"],
+}));
+
+//AUTOLOAD ROUTES
+readdirSync('./routes').map(r => app.use('/api', require(`./routes/${r}`)));
+
+
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`SERVER IS RUNNING ON PORT ${PORT}`));
