259ae6d9b28572e9f24ca63e8f3dc1c6c12901ed
alihassan-here <alihassanuog50@gmail.com>
2022-06-26 00:06:54 +0500

    axios configuration

--- client/context/index.js
+++ client/context/index.js
@@ -1,4 +1,6 @@
 import { useState, createContext, useEffect } from "react";
+import axios from 'axios';
+import { useRouter } from "next/router";
 
 const UserContext = createContext();
 
@@ -7,10 +9,30 @@ const UserProvider = ({ children }) => {
         user: {},
         token: "",
     });
+    const router = useRouter();
 
     useEffect(() => {
         setState(JSON.parse(localStorage.getItem('auth')));
     }, []);
+
+
+    //DEFAULT AXIOS SETTING
+    const token = state && state.token ? state.token : "";
+    axios.defaults.baseURL = process.env.NEXT_PUBLIC_API;
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+    //IF TOKEN GOT EXPIRED THEN WE'LL LOGOUT USER USING AXIOS
+    axios.interceptors.response.use(
+        function (response) {
+            return response;
+        }, function (error) {
+            let res = error.response;
+            if (res.status === 401 && res.config && !res.config.__isRetryRequest) {
+                setState(false);
+                localStorage.removeItem("auth");
+                router.push("/login");
+            }
+        });
 
     return (
         <UserContext.Provider value={[state, setState]}>
